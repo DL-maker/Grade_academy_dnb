7dc2091a61db259db6f620ade25111ae2d86e286
LD-maker <laith.dababi@oteria.fr>
2024-12-02 10:02:53 +0100

    Ajout du squelette initial pour l'application avec une interface graphique basique. - Importation de la librairie . - Création d'une fonction  pour gérer le choix de l'utilisateur entre interface graphique et aucune action. - Implémentation de la fonction  pour afficher une fenêtreز - Placeholder pour la fonction .

---
+++ Academy.py
@@ -0,0 +1,35 @@
+import customtkinter as ct
+
+def main():
+    print(r"""
+ _____               _         ___                _                            _       _
+|  __ \             | |       / _ \              | |                          | |     | |
+| |  \/_ __ __ _  __| | ___  / /_\ \ ___ __ _  __| | ___ _ __ ___  _   _    __| |_ __ | |__
+| | __| '__/ _` |/ _` |/ _ \ |  _  |/ __/ _` |/ _` |/ _ \ '_ ` _ \| | | |  / _` | '_ \| '_ \
+| |_\ \ | | (_| | (_| |  __/ | | | | (_| (_| | (_| |  __/ | | | | | |_| | | (_| | | | | |_) |
+ \____/_|  \__,_|\__,_|\___| \_| |_/\___\__,_|\__,_|\___|_| |_| |_|\__, |  \__,_|_| |_|_.__/
+                                                                    __/ |
+                                                                   |___/  by Laith & Adel """)
+    valeur = input("Tu veux une interfaces graphique ? (Yes/No)")
+    if valeur == "Yes" or valeur == "Y" or valeur == "y" or valeur == "yes":
+        return Accept()
+    return Refuse()
+
+def button_callback(): # Fonction de test
+    print("button clicked")
+
+def Accept(): # Affichage d'une fenetre
+    app = ct.CTk()
+    app.geometry("500x400")
+
+    textbox = ct.CTkTextbox(app, width=400, height=200)
+    textbox.pack(pady=20)
+
+    button = ct.CTkButton(app, text="my button", command=button_callback) # Quand on click elle appel la fonction button_callback()
+    button.pack(padx=20, pady=20)
+
+    app.mainloop()
+
+def Refuse():
+    ...
+print(main())
